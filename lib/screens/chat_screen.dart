6d5fe28694c58c7e9a567efd88fc108e0e5d4b3e
Yassine <80011492+Yassine-ch@users.noreply.github.com>
2022-12-01 23:57:36 +0100

    chatApp

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,73 +3,76 @@ import 'package:chatapp/widgets/chat_buble.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-class ChatScreen extends StatelessWidget {
 
- static String id ='ChatScreen';
+class ChatScreen extends StatelessWidget {
+  static String id = 'ChatScreen';
+
 //call collection
- CollectionReference messages = FirebaseFirestore.instance.collection(KMessgesCollections);
- TextEditingController controller = TextEditingController();
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(KMessgesCollections);
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<DocumentSnapshot>(
-      future: messages.doc('2BCHxeSGbf0rvozCxzLY').get(),
-      builder: (context, snapshot){
-        print(snapshot.data!['msg']);
-        return Scaffold(
-          appBar: AppBar(
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
 
-            automaticallyImplyLeading: false,
-            backgroundColor: Color(0xffC0B3FF),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(keyLog,
-                  height: 50,
-                ),
-                Text('CHAT'),
-              ],
-            ),
-            centerTitle: true,
-          ),
-          body: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(itemBuilder: (context,index)
-                {
-                  return ChatBuble();
-                }
-                ),
+
+        if (snapshot.hasData) {
+          print(snapshot.data!.docs[0]['msg']);
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Color(0xffC0B3FF),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    keyLog,
+                    height: 50,
+                  ),
+                  Text('CHAT'),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: TextField(
-                  controller: controller,
-                  onSubmitted:(data) {
-                    messages.add({
-                      'msg' :data,
-                    });
-                    controller.clear();
-                  },
-                  decoration: InputDecoration(
-                      hintText: 'send Message',
-                      suffixIcon: Icon(
-                        Icons.send,
-                        color:Color(0xffAC29F5) ,
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16)
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      )
+              centerTitle: true,
+            ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(itemBuilder: (context, index) {
+                    return ChatBuble();
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add({
+                        'msg': data,
+                      });
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'send Message',
+                        suffixIcon: Icon(
+                          Icons.send,
+                          color: Color(0xffAC29F5),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        )),
                   ),
                 ),
-              ),
-            ],
-          ),
-
-        );
+              ],
+            ),
+          );
+        } else {
+          return Text('LOADING ...');
+        }
       },
     );
   }
